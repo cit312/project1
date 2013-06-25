1d909b5b13ac41ce0ab53a77ab6b8de0d6e74b89
jasonlf <jt13flannery@gmail.com>
2013-06-25 16:57:52 -0500

    Create weather arraylist.java

---
+++ weather arraylist.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+
+public class ArrayList {
+
+  public HashMap get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+//	public HashMap get(int i) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+}
